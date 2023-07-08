8b491c6c7c47bac63ab826c8206b4a0eeff64dc3
Yveder Joseph <geekblack22@users.noreply.github.com>
2023-07-08 11:15:39 -0400

    Update main.cpp

--- Bike_GPS/src/main.cpp
+++ Bike_GPS/src/main.cpp
@@ -40,7 +40,6 @@ int ledLeftPin = 10; //change values when connected to board
 int ledRightPin = 12; //"
 enum PLAY {FIRST,SECOND,LAST}play;
 boolean done = false;
-float test[59][2] = {{-71.091224,42.274514},{-71.091315,42.274402},{-71.091446,42.27431},{-71.091681,42.274181},{-71.092036,42.274056},{-71.092012,42.273907},{-71.091984,42.273615},{-71.091981,42.273577},{-71.091952,42.273165},{-71.09193,42.272702},{-71.091925,42.272624},{-71.092043,42.272157},{-71.092254,42.271586},{-71.09241,42.271213},{-71.092576,42.270806},{-71.092701,42.270622},{-71.092834,42.270518},{-71.092907,42.270465},{-71.093059,42.270353},{-71.093253,42.270211},{-71.093286,42.270196},{-71.093422,42.270169},{-71.093601,42.270167},{-71.093651,42.270161},{-71.09373,42.27014},{-71.093767,42.270125},{-71.093761,42.270069},{-71.093759,42.270009},{-71.093768,42.26976},{-71.093814,42.26869},{-71.093849,42.268112},{-71.093871,42.267741},{-71.093878,42.267625},{-71.093896,42.267407},{-71.093908,42.267234},{-71.093924,42.26694},{-71.093932,42.266802},{-71.09398,42.266494},{-71.09407,42.266282},{-71.094132,42.266174},{-71.094145,42.266115},{-71.094144,42.266047},{-71.09411,42.265991},{-71.094049,42.265959},{-71.093971,42.265949},{-71.09386,42.265988},{-71.093783,42.266114},{-71.093771,42.266159},{-71.093752,42.266599},{-71.093749,42.266705},{-71.093742,42.266936},{-71.093726,42.267227},{-71.093705,42.267296},{-71.093576,42.267517},{-71.093522,42.267582},{-71.093502,42.267567},{-71.093407,42.267535},{-71.093328,42.267538},{-71.092315,42.267731}};
 void setup() {
   Serial.begin(115200);
   /*allowed.setup();
@@ -56,7 +55,7 @@ hub.setup();
 delay(500);
   Serial.println("\r\nRecord start!\r\n");
   Audio_speech* audio = new Audio_speech();
-//  Audio* audio = new Audio(M5STACKFIRE);
+
   audio_speech->Record();
   CloudSpeechClient* cloudSpeechClient = new CloudSpeechClient(USE_ACCESSTOKEN);
   cloudSpeechClient->Transcribe(audio_speech);
@@ -166,13 +165,10 @@ Serial.println(direction);
 
 
  prox_sensor.detectionAlert();
-  //sprintf(data, "{\"lat\": \"%s\", \"lon\": \"%s\"}", lat_str, lng_str); 
-        //hub.loop(data);
-        //Serial.println(hub.getAdress(lat_str,lng_str));
+  
  while (SerialGPS.available() > 0) {
    prox_sensor.detectionAlert();
-    //sprintf(data, "{\"lat\": \"%s\", \"lon\": \"%s\"}", lat_str, lng_str); 
-        //hub.loop(data);
+    
     if (gps.encode(SerialGPS.read()))
     {
       if (gps.location.isValid())
@@ -184,12 +180,7 @@ Serial.println(direction);
          lcd.setCursor(0, 0);
        // lcd.print("Lat = ");
         lcd.print(hub.getAdress(lat_str,lng_str));
-        //lcd.print(lat_str);
-       // lcd.setCursor(0,1);
-       // lcd.print("Long = ");
-        //lcd.print(lng_str);
-        //sprintf(data, "{\"lat\": \"%s\", \"lon\": \"%s\"}", lat_str, lng_str); 
-        //Blynk.virtualWrite(V0, 1, latitude, longitude, "Location");
+      
 
       }
        lcd.setCursor(16,1);
